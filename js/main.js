85f26e496cb07592e9102338f5b347717a8d5e63
Anna21Tori <anna21toria@gmail.com>
2020-08-18 22:12:32 +0200

    convert rgb to hex

--- js/main.js
+++ js/main.js
@@ -11,7 +11,34 @@ var Main = {
                     col.classList.add("col-div");
                     //change color any "pixel" after click
                     col.addEventListener("click", function(){
-                        this.style.background = document.getElementById("color-input").value;
+                        var tools = document.getElementsByClassName("tools-paint");
+                        for(var i=0;i<tools.length;i++){
+                            if(tools[i].style.color == "deeppink")
+                                if(tools[i].id == "brush"){
+                                    this.style.background = document.getElementById("color-input").value;
+                                    return;
+                                }
+                        }
+                        //convert rgb to hex
+                        var rgb = this.style.backgroundColor;
+                        var sep = rgb.indexOf(",") > -1 ? "," : " ";
+                        rgb = rgb.substr(4).split(")")[0].split(sep);
+                        
+                        var r = (+rgb[0]).toString(16),
+                        g = (+rgb[1]).toString(16),
+                        b = (+rgb[2]).toString(16);
+
+                        if (r.length == 1)
+                            r = "0" + r;
+                        if (g.length == 1)
+                            g = "0" + g;
+                        if (b.length == 1)
+                            b = "0" + b;
+
+                        var hex =  "#" + r + g + b;
+                        
+                        document.getElementById("color-input").value = hex//this.style.backgroundColor;
+                        
                     });
                     row.appendChild(col);
                 }
